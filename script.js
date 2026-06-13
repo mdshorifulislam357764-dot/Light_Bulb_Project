d829418b0418f62244cc3e1289fa120b7a3b9c4b
Md Shoriful Islam <mdshorifulislam357764@gmail.com>
2026-06-13 10:37:23 +0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const bulb = document.getElementById("bulb");
+const switchBtn = document.getElementById("switch");
+
+switchBtn.addEventListener("click", () => {
+    bulb.classList.toggle("on");
+    switchBtn.classList.toggle("off");
+});
